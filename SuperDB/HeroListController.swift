19bf6a14b9929358f2c58c2841a59ce5f9c59991
xxing <953817@qq.com>
2017-02-26 16:54:34 +0800

    Initial Commit

---
+++ SuperDB/HeroListController.swift
@@ -0,0 +1,117 @@
+//
+//  HeroListController.swift
+//  SuperDB
+//
+//  Created by xxing on 2017/2/26.
+//  Copyright © 2017年 xxing. All rights reserved.
+//
+
+import UIKit
+
+class HeroListController:
+UIViewController, UITableViewDataSource, UITableViewDelegate, UITabBarDelegate {
+    
+    let kSelectedTabDefaultsKey = "Selected Tab"
+    
+    enum TabType:Int {
+        case kByName = 0
+        case kBySecretIdentity = 1
+    }
+
+    @IBOutlet weak var heroTableview: UITableView!
+    @IBOutlet weak var heroTabBar: UITabBar!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+         self.navigationItem.leftBarButtonItem = self.editButtonItem
+        
+        let defaults = UserDefaults.standard
+        let selectedTab = defaults.integer(forKey: kSelectedTabDefaultsKey)
+        heroTabBar.selectedItem = self.heroTabBar.items?[selectedTab]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    
+    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        
+        let defaults = UserDefaults.standard
+        let tabIndex = tabBar.items?.index(of: item)
+        defaults.set(tabIndex, forKey: kSelectedTabDefaultsKey)
+    }
+    
+    @IBAction func addHero(_ sender: Any) {
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
